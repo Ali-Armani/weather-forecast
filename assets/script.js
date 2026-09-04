0fb73c30f4f6fbe7da9b7be2919a52d6beaa3729
Ali Armani <armani.ali@proton.me>
2026-09-04 14:47:27 +0330

    feat: define necessary variables

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,6 @@
+const apiKey = 'a8c890da0527aaa95ffefd7d63a88998';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric';
 
+const searchBox = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
+const weatherIcon = document.querySelector('.weather-icon');
